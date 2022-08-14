31344507d5fed0b24f910c1f55dca1211f07627d
bioinagesh <nagzz@users.noreply.github.com>
2022-08-14 19:02:29 +0530

    Update multifilesgread.R

--- multifilesgread.R
+++ multifilesgread.R
@@ -1,1 +1,7 @@
-
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages(library(gread))
+args = commandArgs(trailingOnly=TRUE)
+gtf_file <- file.path(args[1])
+gtf<-read_format(gtf_file)
+ans <- construct_introns(gtf, update=FALSE)[]
+write.table(ans,file="",row.names = FALSE, col.names = FALSE)
