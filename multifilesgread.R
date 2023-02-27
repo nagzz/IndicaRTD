0087ed6d689ff4b4dce0edf2422afb54393c34fc
bioinagesh <nagzz@users.noreply.github.com>
2023-02-27 23:01:48 +0530

    Update multifilesgread.R

--- multifilesgread.R
+++ multifilesgread.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+##https://rdrr.io/github/asrinivasan-oa/gread/src/R/construct-introns.R
 suppressPackageStartupMessages(library(gread))
 args = commandArgs(trailingOnly=TRUE)
 gtf_file <- file.path(args[1])
